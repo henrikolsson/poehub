428ef235c365ca3da6a2cb9ebb54b0957d8a9fb8
Henrik Olsson <henrik@fixme.se>
2015-11-29 22:44:12 +0100

    Fix tests

--- test/poehub/dat_test.clj
+++ test/poehub/dat_test.clj
@@ -6,7 +6,7 @@
 
 (def test-file (.getPath (io/resource "Characters.dat")))
 
-(def expected {"Unknown28" 77 "BaseStrength" 32 "BaseIntelligence" 14 "Unknown17" 2 "Unknown18" "Audio/Dialogue/Character/Str/Intro/Ma_01_Intro.ogg" "Unknown15" 6 "Row" 0 "Unknown27" 0 "Actor" "Metadata/Characters/Str/Str.act" "BaseMaxLife" 50 "BaseDexterity" 14 "Unknown16" 1 "Data0" '(3927) "Unknown14" 0 "Description" "I am a warrior, raised to honour my Ancestors, to die with a weapon in my hand and the Karui Way in my blood. \r\n\r\nOriath chained me, made me its slave. For three years I have lived without my family, my pride, my Way. \r\n\r\nI welcome Exile. I welcome Wraeclast. Hear me, Ancestors! A slave stands behind you. A warrior stands before you. \r\n\r\nAnd Death walks at our side." "Unknown11" 1341 "AnimatedObject" "Metadata/Characters/Str/Str.ao" "Icon" "" "BaseMaxMana" 40 "Unknown12" 0 "Name" "Marauder" "MinDamage" 2 "WeaponSpeed" 833 "MaxDamage" 8 "MaxAttackDistance" 4 "Id" "Metadata/Characters/Str/Str" "Unknown13" 0 "Unknown26" 590 "Unknown6" 1})
+(def expected {"Unknown13" 0, "BaseMaxLife" 50, "ACTFile" "Metadata/Characters/Str/Str.act", "StartWeapon_BaseItemTypesKey" 590, "BaseMaxMana" 40, "Unknown6" 1, "AOFile" "Metadata/Characters/Str/Str.ao", "IntroSoundFile" "Audio/Dialogue/Character/Str/Intro/Ma_01_Intro.ogg", "Unknown15" 6, "Row" 0, "Icon" "", "Keys0" '(3927), "Description" "I am a warrior, raised to honour my Ancestors, to die with a weapon in my hand and the Karui Way in my blood. \r\n\r\nOriath chained me, made me its slave. For three years I have lived without my family, my pride, my Way. \r\n\r\nI welcome Exile. I welcome Wraeclast. Hear me, Ancestors! A slave stands behind you. A warrior stands before you. \r\n\r\nAnd Death walks at our side.", "MinDamage" 2, "CharacterSize" 2, "BaseDexterity" 14, "Unknown28" 77, "WeaponSpeed" 833, "StartSkillGem_BaseItemTypesKey" 1341, "BaseIntelligence" 14, "Unknown16" 1, "Name" "Marauder", "BaseStrength" 32, "MaxAttackDistance" 4, "MaxDamage" 8, "Id" "Metadata/Characters/Str/Str", "Unknown14" 0})
 
 (deftest can-parse
   (let [parsed (first (parse test-file))]
@@ -16,3 +16,4 @@
 
 (deftest can-bytes-to-uint-le
   (is (= 352 (bytes-to-uint-le (into-array Byte/TYPE '(96 01 00 00))))))
+
